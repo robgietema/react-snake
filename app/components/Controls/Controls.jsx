3cc0562e4638c5bb7b14d2bd74069866e0cb57d5
Laurens Kling <laurens@goedideemedia.nl>
2015-07-01 15:39:07 +0200

    control styles

--- app/components/Controls/Controls.jsx
+++ app/components/Controls/Controls.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import './Controls.scss';
 
 class Controls extends React.Component {
   
@@ -13,10 +14,10 @@ class Controls extends React.Component {
     
     return (
       <div className="controls">
-        <button className="left" onClick={this.left.bind(this)}>&#65513;</button>
-        <button className="up" onClick={this.up.bind(this)}>&#65514;</button>
-        <button className="down" onClick={this.down.bind(this)}>&#65516;</button>
-        <button className="right" onClick={this.right.bind(this)}>&#65515;</button>
+        <button className="left" onClick={this.left.bind(this)}>&#9668;</button>
+        <button className="up" onClick={this.up.bind(this)}>&#9650;</button>
+        <button className="down" onClick={this.down.bind(this)}>&#9660;</button>
+        <button className="right" onClick={this.right.bind(this)}>&#9658;</button>
       </div>
     );
   }
@@ -36,8 +37,6 @@ class Controls extends React.Component {
   right() {
     this.props.app.gridActions.setDirection('right');
   }
-  
-  
   
 }
 
